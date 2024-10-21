a96224421c2868a88c451851109d02070d291460
Pai Peng <sipaipv6@gmail.com>
2024-10-21 13:36:08 +0800

    Initial Commit

---
+++ HelloWorldARUITests/HelloWorldARUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  HelloWorldARUITestsLaunchTests.swift
+//  HelloWorldARUITests
+//
+//  Created by Pai Peng on 21.10.24.
+//
+
+import XCTest
+
+final class HelloWorldARUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
