d2d5046f66cd95950e1eb61684c8b9a6422607ae
Pai Peng <sipaipv6@gmail.com>
2024-10-22 15:04:01 +0800

    request for qr detection

--- HelloWorldAR/ViewController.swift
+++ HelloWorldAR/ViewController.swift
@@ -13,6 +13,11 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    var qrRequests = [VNRequest]()
+    var detectedDataAnchor: ARAnchor?
+    var processing = false
+    var latestFrame: ARFrame?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -22,6 +27,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
+        self.startQrCodeDetection()
         
         // Create a new scene
         let scene = SCNScene(named: "art.scnassets/watch1usdc.scn")!
@@ -72,15 +78,91 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         // Reset tracking and/or remove existing anchors if consistent tracking is required
         
     }
+    
+    func startQrCodeDetection() {
+        // Create a Barcode Detection Request
+        let request = VNDetectBarcodesRequest(completionHandler: self.requestHandler)
+        // Set it to recognize QR code only
+        request.symbologies = [.QR]
+        self.qrRequests = [request]
+    }
+    
+    func requestHandler(request: VNRequest, error: Error?) {
+        // Get the first result out of the results, if there are any
+        if let results = request.results, let result = results.first as? VNBarcodeObservation {
+            guard let payload = result.payloadStringValue else {return}
+            // Get the bounding box for the bar code and find the center
+            var rect = result.boundingBox
+            // Flip coordinates
+            rect = rect.applying(CGAffineTransform(scaleX: 1, y: -1))
+            rect = rect.applying(CGAffineTransform(translationX: 0, y: 1))
+            // Get center
+            let center = CGPoint(x: rect.midX, y: rect.midY)
+
+            DispatchQueue.main.async {
+                self.hitTestQrCode(center: center)
+                self.processing = false
+            }
+        } else {
+            self.processing = false
+        }
+    }
+
+    func hitTestQrCode(center: CGPoint) {
+        print("hitTestQrCode: \(center)")
+        if let hitTestResults = self.latestFrame?.hitTest(center, types: [.featurePoint] ),
+            let hitTestResult = hitTestResults.first {
+            if let detectedDataAnchor = self.detectedDataAnchor,
+                let node = self.sceneView.node(for: detectedDataAnchor) {
+                let previousQrPosition = node.position
+                node.transform = SCNMatrix4(hitTestResult.worldTransform)
+
+            } else {
+                // Create an anchor. The node will be created in delegate methods
+                self.detectedDataAnchor = ARAnchor(transform: hitTestResult.worldTransform)
+                self.sceneView.session.add(anchor: self.detectedDataAnchor!)
+            }
+        }
+    }
+    
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+
+        // If this is our anchor, create a node
+        if self.detectedDataAnchor?.identifier == anchor.identifier {
+            let sphere = SCNSphere(radius: 1.0)
+            sphere.firstMaterial?.diffuse.contents = UIColor.red
+            let sphereNode = SCNNode(geometry: sphere)
+            sphereNode.transform = SCNMatrix4(anchor.transform)
+            return sphereNode
+        }
+        return nil
+    }
 }
 
 extension ViewController : ARSessionDelegate {
     func session(_ session: ARSession, didUpdate frame: ARFrame) {
         print("session didUpdate:")
+        self.latestFrame = frame
+        /*
         let capturedImage: CVPixelBuffer = frame.capturedImage
         var image = UIImage(pixelBuffer: capturedImage)
         if image != nil {
             image = nil
+        }
+         */
+        
+        do {
+            if self.processing {
+              return
+            }
+            self.processing = true
+            // Create a request handler using the captured image from the ARFrame
+            let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: frame.capturedImage,
+                                                            options: [:])
+            // Process the request
+            try imageRequestHandler.perform(self.qrRequests)
+        } catch {
+
         }
     }
 }
